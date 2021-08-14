006056b6075e759ba58357fe115e1443563f15f2
SHU0744 <62059397+SHU0744@users.noreply.github.com>
2021-08-14 19:26:01 +0900

    styely　jsの書き方

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@
 import React from "react";
 
 // コンポーネント（jsx基本 jsの中にhtml5を記入）
+// コンポーネント名は必ず先頭を大文字から初める。
 const App = () => {
+  // 関数
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "20px"
+  };
+
   return (
     // 一つのタグに必ずすること
     //React.Fragmentはdom上ではタグとして認識されない。divを生成したくないときに使用
     <React.Fragment>
-      <h1>これがjsx基本です</h1>
-      <p>一つのタグの中にすること</p>
+      <h1 style={{ color: "red" }}>これがjsx基本です</h1>
+      <p style={contentStyle}>一つのタグの中にすること</p>
+      <button onClick={onClickButton}>ボタン</button>
     </React.Fragment>
   );
 };
